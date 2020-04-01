cfc17a2c87f5fe1c61d707e6be9156b9027dc02c
mahmoud98-30 <55198262+mahmoud98-30@users.noreply.github.com>
2020-04-01 23:53:42 +0200

    new one

--- Src/blog/views.py
+++ Src/blog/views.py
@@ -1,4 +1,42 @@
 from django.shortcuts import render
-
+posts = [
+    {
+        'title':'التدوينة الاولة',
+        'content':'نص التدوينة الاولة كنص تجريبي',
+        'post_date':'16-3-2020',
+        'author':'محمود أحمد الفوراوي',
+    },
+    {
+        'title':'التدوينة الثانية',
+        'content':'نص التدوينة الثانية كنص تجريبي',
+        'post_date':'16-9-2020',
+        'author':'محمود أحمد الفوراوي',
+    },
+    {
+        'title':'التدوينة الثالتة',
+        'content':'نص التدوينة الثالتة كنص تجريبي',
+        'post_date':'16-10-2020',
+        'author':'محمود أحمد الفوراوي',
+    },
+]
 def home(request):
-    return render(request,'blog/index.html',{'title':'Home'})
+    context = {
+        'title': 'الصفحة الرئيسية',
+        'posts': posts,
+    }
+    return render(request, 'blog/index.html', context)
+def about(request):
+    return render(request, 'blog/about.html', {'title':'من أنا'})
+
+
+
+
+
+
+
+
+
+
+
+
+
